2e14ee8d66f0151865b024c17b625f6a3f8f6455
Declan Cook <DeclanCook44@gmail.com>
2014-02-28 23:33:06 +0000

    remove database from git, use update.py to add data into database

--- model/models.py
+++ model/models.py
@@ -1,13 +1,38 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey
 from database import Base
 
 class Channel(Base):
     __tablename__ = 'channels'
-    id = Column(Integer, primary_key=True)
+    channelId = Column(Integer, primary_key=True)
     name = Column(String(50), unique=True)
+    rtid = Column(Integer, unique=True)
 
-    def __init__(self, name=None):
+    def __init__(self, name=None, rtid=None):
         self.name = name
+        self.rtid = rtid
 
     def __repr__(self):
         return '<Name %r>' % (self.name)
+
+class Show(Base):
+    __tablename__ = 'show'
+    id = Column(Integer, primary_key=True)
+    channelId = Column(Integer, ForeignKey("channels.channelId"))
+    title = Column(String(50), unique=False)
+    startTime = Column(DateTime)
+    endTime = Column(DateTime)
+    description = Column(String(400))
+    isNewSeries = Column(Boolean)
+    isRepeat = Column(Boolean)
+
+    def __init__(self, channelId, title=None, startTime=None, endTime=None, description=None, isNewSeries=None, isRepeat=None):
+        self.channelId = channelId
+        self.title = title
+        self.startTime = startTime
+        self.endTime = endTime
+        self.description = description
+        self.isNewSeries = isNewSeries
+        self.isRepeat = isRepeat
+
+    def __repr__(self):
+        return '<title %r>' % (self.title)
